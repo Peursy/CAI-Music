e575397f0c47d460f2307db217a2966077431df8
SAOUDI Amina <a8saoudi@menthe19.enib.fr>
2019-10-15 14:49:03 +0200

    mainSakura.js added sakura_point_vsh.js added SignUpPage.html modified SignUpPage.js modified

--- Home_UI/src/js/SignUpPage.js
+++ Home_UI/src/js/SignUpPage.js
@@ -1,6 +1,13 @@
-
-
-function submit(){
-	let username = document.getElementById("username_field")
-	console.log(username.text)
+// Utilities
+// =======================================================
+ function check() {
+ if(document.getElementById('password2').value !=''){
+  if ((document.getElementById('password1').value ==
+    document.getElementById('password2').value)) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+  }}
 }
