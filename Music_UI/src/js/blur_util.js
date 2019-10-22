6d4b673f8ca8a3cf4fab815437d95e5a432038b1
ShinySilver <nicolas.louismarie@gmail.com>
2019-10-22 14:17:55 +0200

    Improved CSS

--- Music_UI/src/js/blur_util.js
+++ Music_UI/src/js/blur_util.js
@@ -1,3 +1,7 @@
-blur = function(){
-  console.log("Blured!");
+blur = function(name){
+  console.log("Blured "+name+"!\n");
+  e = document.getElementById(name);
+  p=e.parentNode;
+  c=document.createElement("div class='center'");
+  p.write("BWAAAAAAAAAA");
 }
