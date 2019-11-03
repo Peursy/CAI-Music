0fa483bca2bde57265794825fc997db7645e99ba
WexyR <romain.biannic2@gmail.com>
2019-11-03 12:57:48 +0100

    add blur when needed on all pages

--- Music_UI/src/js/blur_util.js
+++
@@ -1,7 +0,0 @@
-blur = function(name){
-  console.log("Blured "+name+"!\n");
-  e = document.getElementById(name);
-  p=e.parentNode;
-  c=document.createElement("div class='center'");
-  p.write("BWAAAAAAAAAA");
-}
